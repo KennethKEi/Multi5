a08d52f8d73ba71e5ccdf80757b45b0bc7484642
Kenneth_KEi <hotdinner@gmail.com>
2020-09-26 17:11:02 +1000

    Added int for Red, blue and Yellow character

---
+++ Multi5/Assets/InfoGamerAssets/PUN2_Matchmaking/_General/Scripts/CharacterSelection.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+
+    public static CharacterSelection CS;
+    public int myCharacter;
+
+    private void OnEnable()
+    {
+        if(CharacterSelection.CS ==null)
+        {
+            CharacterSelection.CS = this;
+        }
+        else
+        {
+            if(CharacterSelection.CS !=this)
+            {
+                Destroy(CharacterSelection.CS.gameObject);
+                CharacterSelection.CS = this;
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void BlueCharacter()
+    {
+        myCharacter = 0;
+        Debug.Log("myCharacter is " + myCharacter);
+    }
+    public void RedCharacter()
+    {
+        myCharacter = 1;
+        Debug.Log("myCharacter is " + myCharacter);
+    }
+    public void YellowCharacter()
+    {
+        myCharacter = 3;
+        Debug.Log("myCharacter is " + myCharacter);
+    }
+    
+}
